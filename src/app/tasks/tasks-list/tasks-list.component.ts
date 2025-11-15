39413f8cb5c125af2777f3aaa0702924fe7ba329
mohanad-salameh <124061910+mohanad-salameh@users.noreply.github.com>
2025-11-15 14:11:48 +0300

    Add files via upload

---
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, computed, inject, signal } from '@angular/core';
+
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-tasks-list',
+  standalone: true,
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css',
+  imports: [TaskItemComponent],
+})
+export class TasksListComponent {
+  private tasksService=inject (TasksService);
+  private selectedFilter = signal<string>('all');
+  tasks = computed(()=>{
+    switch(this.selectedFilter()){
+     
+      case 'open':
+            return this.tasksService.allTasks().filter(x=>x.status==='OPEN');
+       case 'in-progress' :
+            return this.tasksService.allTasks().filter(x=>x.status==='IN_PROGRESS');
+       case 'done':
+             return this.tasksService.allTasks().filter(x=>x.status==='DONE');
+             default :return this.tasksService.allTasks();
+
+
+    }
+   
+
+  });
+
+
+  onChangeTasksFilter(filter: string) {
+    this.selectedFilter.set(filter);
+  }
+}
